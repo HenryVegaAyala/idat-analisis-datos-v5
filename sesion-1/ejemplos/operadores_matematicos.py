6128e06a64c4680b568128a24873d9c1ee4e4e7b
Henry Vega <hp.vega@hotmail.com>
2026-06-08 21:53:16 -0500

    add basic arithmetic operations in operadores_matematicos.py

---
+++ sesion-1/ejemplos/operadores_matematicos.py
@@ -0,0 +1,17 @@
+# Operadores matemáticos
+
+# aritméticos
+suma = 5 + 5
+restar = 120 - 60
+multiplicacion = 10 * 10
+division = 15 / 5
+
+# Aritméticos directos
+numero = 10
+numero += 5 # ===> esto es igual a (numero = numero + 5)
+
+numero = 10
+numero -= 5 # ===> esto es igual a (numero = numero - 5)
+
+numero = 10
+numero += -5 # ===> esto es igual a (numero = numero + -5)
