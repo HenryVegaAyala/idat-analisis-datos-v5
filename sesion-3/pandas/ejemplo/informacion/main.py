8b56f31bb7bdc7cd63d0a8d19f34cc4fc2ca835f
Henry Vega <hp.vega@hotmail.com>
2026-06-15 21:34:11 -0500

    add student average calculation functionality

---
+++ sesion-3/pandas/ejemplo/informacion/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_excel("ventas_enero_excel.xlsx")
+
+print(df.info())
---
+++ sesion-3/pandas/ejemplo/vistazo/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("ventas_enero_csv.csv")
+
+print(df.head(2))
