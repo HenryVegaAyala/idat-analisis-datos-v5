8c42f3591645d3c6507268c5c91e2a68371be683
Henry Vega <hp.vega@hotmail.com>
2026-06-22 21:54:45 -0500

    add CSV file processing and annual consolidation to main.py

---
+++ sesion-4/graficos/tipos/eoverlays/main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from pydantic import color
+
+mes = ["Enero", "Febrero", "Marzo", "Abril"]
+luz = [40, 50, 45, 60]
+agua = [25, 30, 31, 35]
+
+plt.plot(mes, luz, label="servicios de Luz", marker="o", linewidth=3, color="blue")
+plt.plot(mes, agua, label="servicios de agua", marker="s", linewidth=3, color="red")
+
+plt.title("Gastos de la casa")
+plt.xlabel("Mes")
+plt.ylabel("Gastos S/.")
+
+plt.grid(True, linestyle="--", color="k", linewidth=0.3)
+
+for index, valor in enumerate(luz):
+    plt.text(mes[index], valor + 1, str(valor), ha="center")
+
+for index, valor in enumerate(agua):
+    plt.text(mes[index], valor + 1, str(valor), ha="center")
+
+plt.show()
---
+++ sesion-4/graficos/tipos/histograma/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+edades = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 50]
+
+plt.hist(edades, bins=5, edgecolor="black", color="red")
+
+plt.title("Histograma de edades")
+plt.xlabel("Edades")
+plt.ylabel("Frecuencia")
+
+plt.grid(axis="y", linestyle="--", alpha=0.5)
+plt.tight_layout()
+
+plt.show()
