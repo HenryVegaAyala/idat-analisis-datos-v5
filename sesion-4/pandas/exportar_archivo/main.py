b223f018a8de6604db152e3f75abc8e9989a9725
Henry Vega <hp.vega@hotmail.com>
2026-06-22 20:45:08 -0500

    add data manipulation functionality to main.py

--- sesion-4/pandas/exportar_archivo/main.py
+++ sesion-4/pandas/exportar_archivo/main.py
@@ -1,3 +1,5 @@
+import pprint
+
 import pandas as pd
 
 facturacion = pd.read_csv("facturacion.csv")
@@ -7,4 +9,7 @@ consolidado = pd.merge(facturacion, cliente, on="id_cliente", how="left")
 
 agrupador = consolidado.groupby(["nombre", "pais"]).sum()["monto_total"]
 
-# agrupador
+print(agrupador)
+
+agrupador.to_excel("agrupador.xlsx")
+agrupador.to_csv("agrupador.csv")
