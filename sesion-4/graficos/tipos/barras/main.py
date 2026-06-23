f492b8b042460f58f1e718bb0acb31ed3e359688
Henry Vega <hp.vega@hotmail.com>
2026-06-22 21:34:41 -0500

    add CSV file processing and annual consolidation to main.py

---
+++ sesion-4/graficos/tipos/barras/main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+ciudades = ["Lima", "Cusco", "Arequipa"]
+ventas = [500, 300, 450]
+
+barras = plt.bar(ciudades, ventas)
+
+# Títulos y etiquetas
+plt.title("Ventas por ciudad", fontsize=16, fontweight="bold")
+plt.xlabel("Ciudades")
+plt.ylabel("Ventas")
+
+# Agregar valores encima de cada barra
+for barra in barras:
+    altura = barra.get_height()
+    plt.text(
+        barra.get_x() + (barra.get_width() / 2),
+        altura + 5,
+        f"{altura}",
+        ha='center',
+        fontsize=11,
+    )
+
+# Mejora visual
+plt.grid(axis="y", linestyle="--", alpha=0.5)
+plt.tight_layout()
+
+plt.show()
